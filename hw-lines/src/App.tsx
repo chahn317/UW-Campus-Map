8918e1b14725bf6c7e4c6579205ef10cdacfb312
Christina Hahn <chahn317@uw.edu>
2023-08-09 18:35:46 -0700

    basic

--- hw-lines/src/App.tsx
+++ hw-lines/src/App.tsx
@@ -12,38 +12,41 @@
 import React, { Component } from "react";
 import EdgeList from "./EdgeList";
 import Map from "./Map";
+import Edge from "./Edge";
 
 // Allows us to write CSS styles inside App.css, any styles will apply to all components inside <App />
 import "./App.css";
 
-interface AppState {}
+interface AppState {
+    edges: Edge[]
+}
 
 class App extends Component<{}, AppState> { // <- {} means no props.
 
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      // TODO: store edges in this state
-    };
-  }
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            edges: []
+        };
+    }
 
-  render() {
-    return (
-      <div>
-        <h1 id="app-title">Line Mapper!</h1>
-        <div>
-          {/* TODO: define props in the Map component and pass them in here */}
-          <Map />
-        </div>
-        <EdgeList
-          onChange={(value) => {
-            // TODO: Modify this onChange callback to store the edges in the state
-            console.log("EdgeList onChange", value);
-          }}
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1 id="app-title">Line Mapper!</h1>
+                <div>
+                    {/* DONE: define props in the Map component and pass them in here */}
+                    <Map edges={this.state.edges}/>
+                </div>
+                <EdgeList
+                    onChange={(value) => {
+                        // DONE: Modify this onChange callback to store the edges in the state
+                        this.setState({edges: value})
+                    }}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- hw-lines/src/EdgeList.tsx
+++ hw-lines/src/EdgeList.tsx
@@ -10,18 +10,32 @@
  */
 
 import React, {Component} from 'react';
+import Edge from "./Edge";
+
+const start = "Replace this text with an edge in the following format on each " +
+                     "line: \nx1 y1 x2 y2 color"
 
 interface EdgeListProps {
-    onChange(edges: any): void;  // called when a new edge list is ready
-                                 // TODO: once you decide how you want to communicate the edges to the App, you should
+    onChange(edges: Edge[]): void;  // called when a new edge list is ready
+                                 // DONE: once you decide how you want to communicate the edges to the App, you should
                                  // change the type of edges so it isn't `any`
+}
+
+interface EdgeListState {
+    text: string;
 }
 
 /**
  * A text field that allows the user to enter the list of edges.
  * Also contains the buttons that the user will use to interact with the app.
  */
-class EdgeList extends Component<EdgeListProps> {
+class EdgeList extends Component<EdgeListProps, EdgeListState> {
+
+    constructor(props: EdgeListProps) {
+        super(props);
+        this.state = {text: start}
+    }
+
     render() {
         return (
             <div id="edge-list">
@@ -29,11 +43,42 @@ class EdgeList extends Component<EdgeListProps> {
                 <textarea
                     rows={5}
                     cols={30}
-                    onChange={() => {console.log('textarea onChange was called');}}
-                    value={"I'm stuck..."}
+                    value={this.state.text}
+                    onChange={(rawText ) => {
+                        this.setState({text: rawText.target.value});
+                    }}
                 /> <br/>
-                <button onClick={() => {console.log('Draw onClick was called');}}>Draw</button>
-                <button onClick={() => {console.log('Clear onClick was called');}}>Clear</button>
+                <button onClick={() => {
+                    const edges: Edge[] = []
+                    const lines: string[] = this.state.text.trim().split("\n")
+                    for (let i = 0; i < lines.length; i++) {
+                        const tokens: string[] = lines[i].trim().split(" ")
+                        if (tokens.length != 5) {
+                            alert("Edges must be in the format: x1 y1 x2 y2 color")
+                            break;
+                        }
+                        const coordinates: number[] = []
+                        for (let i = 0; i < tokens.length - 1; i++) {
+                            const num: number = parseInt(tokens[i])
+                            if (num < 0 || num > 4000) {
+                                alert("Coordinates should be between 0 and 4000 inclusive")
+                                break;
+                            }
+                            coordinates.push(num)
+                        }
+                        const edge: Edge = {x1: coordinates[0],
+                            y1: coordinates[1],
+                            x2: coordinates[2],
+                            y2: coordinates[3],
+                            color: tokens[4]}
+                        edges.push(edge)
+                    }
+                    this.props.onChange(edges)
+                }}>Draw</button>
+                <button onClick={() => {
+                    this.setState({text: ""})
+                    this.props.onChange([])
+                }}>Clear</button>
             </div>
         );
     }
